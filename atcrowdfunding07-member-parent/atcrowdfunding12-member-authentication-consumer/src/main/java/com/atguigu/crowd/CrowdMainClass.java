55b7b03fe738bab6cfbf0b2316a251dd186b9a59
hgk <123>
2021-03-10 22:51:33 +0800

    2021.3.10/22:51/前台环境搭建完成

--- atcrowdfunding07-member-parent/atcrowdfunding12-member-authentication-consumer/src/main/java/com/atguigu/crowd/CrowdMainClass.java
+++ atcrowdfunding07-member-parent/atcrowdfunding12-member-authentication-consumer/src/main/java/com/atguigu/crowd/CrowdMainClass.java
@@ -1,10 +1,10 @@
 package com.atguigu.crowd;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient // 当前版本可以不写
+//@EnableDiscoveryClient // 当前版本可以不写
 @SpringBootApplication
 public class CrowdMainClass {
 
--- atcrowdfunding07-member-parent/atcrowdfunding12-member-authentication-consumer/src/main/java/com/atguigu/crowd/handler/PortalHandler.java
+++ atcrowdfunding07-member-parent/atcrowdfunding12-member-authentication-consumer/src/main/java/com/atguigu/crowd/handler/PortalHandler.java
@@ -2,6 +2,7 @@ package com.atguigu.crowd.handler;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
